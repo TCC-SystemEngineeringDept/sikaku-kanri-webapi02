f099aa66dbc53f27036733c7e3a2b364f03d5855
yoshzawa <yoshzawa@gmail.com>
2024-01-12 13:41:36 +0900

    パラメーター調整

--- app/exam.py
+++ app/exam.py
@@ -9,14 +9,19 @@ Exams = [
 ]
 
 @app.get("/list")
-def get_exam_list():
+def get_exam_list(token:str):
     return Exams
 
 @app.get("/{ID}")
-def get_exam_item(ID:str):
+def get_exam_item(ID:str,token:str):
     if ID == "FE00":
         return Exams[0]
     elif ID == "OR00":
         return Exams[1]
     else:
         return {}
+
+@app.post("/add")
+def add_exam_item(ID:str,NAME:str,token:str):
+    return {"message": "Exam added successfully", "exam": {"ID": "FE00", "NAME": "基本情報技術者試験"}}
+    
