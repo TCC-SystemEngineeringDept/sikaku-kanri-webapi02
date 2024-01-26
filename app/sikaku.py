762ee529798b4e516503b49626b6a1bcad83fede
JC-21\210588 <210588@jc-21.jp>
2024-01-26 15:02:14 +0900

    資格修正

--- app/sikaku.py
+++ app/sikaku.py
@@ -2,29 +2,40 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# 資格リストのデータ
-Passed = [
-    {"ID": "FE00", "NAME": "基本情報技術者試験", "DATE": "2022/06/18"},
-    {"ID": "OR00", "NAME": "Java SE Bronze", "DATE": "2023/02/20"}
+# 商品リストのデータ
+vouchers = [
+    {"ID": "FESG", "NAME": "FE/SG受験バウチャー", "DATE": "2024/06/20"},
+    {"ID": "OR00", "NAME": "Oracle認定資格ピアソンVUE 配信監督なし試験用", "DATE": "2023/12/25"}
 ]
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close():
+
 @app.get("/list")
-def get_passed_list(token:str):
-    return Passed
+def get_sikaku_list(token:str Session = Depends(get_db)):
+    sikaku = db.query(Sikaku).all()
+    return sikaku
 
 @app.get("/{ID}")
-def get_passed_item(ID:str,token:str):
-    if ID == "FE00":
-        return Passed[0]
-    elif ID == "OR00":
-        return Passed[1]
+def get_sikaku_item(ID:str,token:str):
+    sikaku = db.query(Sikaku).filter(Sikaku)
+
+    if Sikaku:
+        return {"sikaku_id": Sikaku.sikaku_id,"sikaku_name": Sikaku.sikaku_name}
     else:
-        return {}
+        return HTTPException(status_code=484,datail="そんな資格はないよ")
 
 @app.post("/add")
-def add_sikaku(sikaku_id: Integer,sikaku_name: Varchar, db: Session = Depends(get_db)):
-    new_sikaku = sikaku(sikaku_id=sikaku_id, sikaku_name=sikaku_name)
-    db.add(new_sikaku)
+def add_sikaku_item(ID:str,NAME:str,token:str,db: Session = Depends(get_db)):
+    new_sikaku = Sikaku(sikaku_id=ID, sikaku_name=NAME)
+    if new_sikaku == "":
+        return {"message":  "空なのでエラー"}
+else:
+    db.add(sikaku)
     db.commit()
     db.refresh(new_sikaku)
-    return {"message": "Passed was added successfully", "Passed": {{"ID": "FE00", "DATE": "2022/06/18"},}}
+	    return {"message": "voucher was added successfully", "voucher": {{"voucher_id": new_voucher.voucher_id ,"voucher_name" :new_voucher.voucher_name, "voucher_date": new_voucher.voucher_date}}}
